fc729f1d58a7e1f50117bca4eb55202f021f227b
Ron Martin <ronmartin89@gmail.com>
2020-04-08 11:58:10 -0700

    Inputbox done. Wiring up our App.js to our other ocmpnoents" git push "

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import LinearSearch from './LinearSearch';
 import BinarySearch from './BinarySearch';
+import Dataset from './Dataset';
+import InputBox from './InputBox';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-       <LinearSearch />
-       <BinarySearch />
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    search: 0
+  }
+
+  updateSearch = (event) => {
+    this.setState({
+        search: event.target.value
+    })
+  }
+
+  render() {
+    return (
+        <div className="App">
+          <header className="App-header">
+           <InputBox handleSearch={this.updateSearch}/>
+           <LinearSearch searchInput={this.state.search}/>
+           <BinarySearch searchInput={this.state.search}/>
+           <Dataset />
+          </header>
+        </div>
+      );
+  }
 }
 
 export default App;
---
+++ src/InputBox.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+
+export default class InputBox extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    // handleSearch = (event) => {
+    //     this.setState({
+    //         search: event.target.value
+    //     })
+    // }
+    render() {
+        return (
+            <div> 
+                <input onChange = {this.handleSearch} 
+                type='text' 
+                placeholder='Input number to search'
+                id='number'
+                name='number'>
+                </input>
+            </div>
+
+        )
+    }
+}
+
+
+{/* <form className='CreateSpellPage__form' onSubmit={e => this.handleSubmit(e)}>
+            <h2>Create a New Spell</h2>
+            <div className='form-group'>
+              <label htmlFor='add-spell-name'>Spell Name </label>
+              <input
+                type='text'
+                className='CreateSpellPage__control'
+                // on submit, the name will what the server will get data keys name after
+                name='add-spell-name'
+                id='add-spell-name'
+                 // Each time the user types anything into input, we update the state. This allows real-time error messages (ie before submit)
+                onChange={e => this.updateSpell(e.target.value, 'name')}
+                required
+                />
+              {/* Conditional rendering depending on whether the user has changed the input or not */}
+            //   {this.state.name.touched && (<ValidationError message={nameError} />)}
+            // </div>
+
+            
+//   updateSpell(value, keyName) { */}
+//     // Whenever the user types anything into the input, we update the state! each letter at a time!
+//     // When the user types ANYTHING we set the value of touched to "true" from "false" this way we know
+//     // when to correctly render our ValidationError
+//     let newState = { [keyName]: {value, touched: true}};
+//     this.setState(newState);
+//   }
